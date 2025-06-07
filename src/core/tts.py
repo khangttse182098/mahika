3ac731f338e7c0df6e146158558667d3694604cc
khangttse182098@fpt.edu.vn <khangttse182098@fpt.edu.vn>
2025-06-07 16:57:41 +0700

    fix: requirements.txt

--- src/core/tts.py
+++ src/core/tts.py
@@ -1,16 +1,32 @@
 from gtts import gTTS
-from playsound import playsound
 import tempfile
 import os
+import pygame
+import threading
 
-class Tts():
+class Tts:
+    _current_channel = None
+    _lock = threading.Lock()
+
     @staticmethod
     def play_sound(msg, lang="vi"):
         tts = gTTS(msg, lang=lang)
-        with tempfile.NamedTemporaryFile(delete=False, suffix='.mp3') as temp_file: 
-            temp_file_path = temp_file.name
-            tts.save(temp_file_path)
-        
-        playsound(temp_file_path)
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_file:
+            temp_path = temp_file.name
+            tts.save(temp_path)
 
-        os.remove(temp_file_path)
+        def _play():
+            pygame.mixer.init()
+            with Tts._lock:
+                if Tts._current_channel is not None:
+                    Tts._current_channel.stop()
+                pygame.mixer.music.load(temp_path)
+                pygame.mixer.music.play()
+                Tts._current_channel = pygame.mixer.music
+
+            while pygame.mixer.music.get_busy():
+                continue
+
+            os.remove(temp_path)
+
+        threading.Thread(target=_play, daemon=True).start()
