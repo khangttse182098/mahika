55ee79daea9b8e58ac297fe0c74bbaebfba7e883
khangttse182098@fpt.edu.vn <khangttse182098@fpt.edu.vn>
2025-06-07 09:04:38 +0700

    feat: change hover state of fileTile

--- src/ui/file_list.py
+++ src/ui/file_list.py
@@ -2,8 +2,7 @@ import customtkinter as ctk
 class FileTile(ctk.CTkFrame):
     def __init__(self, master, fg="#FFFFFF", isNormal=True, name="", date="", isHovered=False):
         self.isHovered = isHovered
-        self.borderThickness = 2 if self.isHovered else 0
-        super().__init__(master, width=100, height=100, fg_color=fg, border_color="#103098", border_width=self.borderThickness)
+        super().__init__(master, width=100, height=100, fg_color=fg)
         if isNormal == False:
             plusBtn = ctk.CTkLabel(self, text="+", font=ctk.CTkFont(size=50))
             plusBtn.place(relx=0.5, rely=0.5, anchor="center")
@@ -12,17 +11,23 @@ class FileTile(ctk.CTkFrame):
             nameLabel.place(relx=0.5, rely=0.5, anchor="center")
             timeLabel = ctk.CTkLabel(self, text=date, font=ctk.CTkFont(size=8), text_color="#000000")
             timeLabel.place(relx=0.5, rely=0.7, anchor="center")
+    
+    def set_hovered(self):
+        if self.isHovered:
+            self.configure(border_width=0)
+            self.isHovered = False
+        else:
+            self.configure(border_color="#2433BB", border_width=2) 
+            self.isHovered = True
 
 class FileList(ctk.CTk):
     def __init__(self):
         super().__init__(fg_color="#E5E5E5")
         self.title("Trang tài liệu")
+        self.tileList = []
+        self.bind("<Key-l>", self.changeHoveredTile)
         self.geometry("1000x500")
-        self.hoveredPosition = {
-            "row": 0,
-            "col": 1
-        }
-        self.bind("<KeyPress-l>", self.updateHoveredPosition)
+        self.hoveredPosition = 0
         self.max_col = 4
         col = 1
         row = 0
@@ -36,19 +41,19 @@ class FileList(ctk.CTk):
             if col == self.max_col:
                 col = 0 
                 row += 1
-            if col == self.hoveredPosition["col"] and row == self.hoveredPosition["row"]:
-                fileTile = FileTile(self, name="Untitled", date="06/06/2025", isHovered=True)
-            else:
-                fileTile = FileTile(self, name="Untitled", date="06/06/2025", isHovered=False)
-                print("No hovered")
+
+            fileTile = FileTile(self, name="Untitled", date="06/06/2025")
+
+            # add to tileList
+            self.tileList.append(fileTile)
             fileTile.grid(row=row, column=col, padx=10, pady=10)
             col += 1 
-
-    def updateHoveredPosition(self, event):  
-        if self.hoveredPosition["col"] == self.max_col - 1:
-            self.hoveredPosition["col"] = 0 
-            self.hoveredPosition["row"] += 1
-        else:
-            self.hoveredPosition["col"] += 1
-
-        print(f"Hover position: {self.hoveredPosition}")
+        
+        self.tileList[0].set_hovered()
+        
+    
+    def changeHoveredTile(self, event):
+        self.tileList[self.hoveredPosition].set_hovered()
+        self.hoveredPosition += 1
+        print(f"pos: {self.hoveredPosition}")
+        self.tileList[self.hoveredPosition].set_hovered()
